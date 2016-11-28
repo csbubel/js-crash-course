206d51f84a397abef24af97038a73a3b7c526ada
Chris Bubel <christopher.s.bubel@gmail.com>
2016-11-28 14:22:27 -0500

    Updated user spec

--- jasmine/spec/UserSpec.js
+++ jasmine/spec/UserSpec.js
@@ -1,7 +1,7 @@
 describe("User", function() {
     it("should create a user with name and age properties by passing in json", function() {
         const u = new User({name: "John", age: 22});
-        expect(u.name).not.toBe(undefined);
-        expect(u.age).not.toBe(undefined);
+        expect(u.name).toBe("John");
+        expect(u.age).toBe(22);
     });
 });
